b4c4b8b13f36814c95338d747f922b99e624099d
fiqhbdr <fiqhbdr@gmail.com>
2026-07-31 17:29:26 +0700

    feat: Markdown Viewer card uses README preview image

--- app/components/sections/ProjectsSection.tsx
+++ app/components/sections/ProjectsSection.tsx
@@ -79,7 +79,7 @@ const projects = {
       lang: "Swift",
       langColor: "#ff4500",
       stars: 1,
-      img: "https://opengraph.githubassets.com/1/fiqihbadrian/Markdown-Viewer",
+      img: "/images/projects/markdown-viewer.png",
     },
     {
       name: "MyUang",
